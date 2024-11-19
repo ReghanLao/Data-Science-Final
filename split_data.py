74f807ff8913f1f75ebf1161430374beadd7f1e5
Reghan Lao <reginald6688@gmail.com>
2024-11-19 17:40:12 -0500

    comment for clarity

--- split_data.py
+++ split_data.py
@@ -8,7 +8,7 @@ def split_data(data, test_size=0.2):
 
   for user_id, group in groups:
     split_index = int(len(group) * (1-test_size))
-    group = group.sample(frac=1)
+    group = group.sample(frac=1) #randomize order of ratings
     train.append(group.iloc[:split_index])
     test.append(group.iloc[split_data:])
 
