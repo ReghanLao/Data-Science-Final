60f8ae36d6a253265a79aac703cf025fc0a9936a
Reghan Lao <reginald6688@gmail.com>
2024-11-20 18:49:20 -0500

    changed main

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from load_data import load_data
 from clean_data import clean_data
+from rating_prediction import create_user_item_matrx
 
-data = load_data("Sports_and_Outdoors_5.json")
-
-#Inspecting data & brief overview on dataset 
-print("First Few Rows:")
-print(data.head())
-
-print("\nDataset Info:")
-print(data.info())  
-
-print("\nStatistical Summary:")
-print(data.describe()) 
-
-print("\nMissing Values:")
-print(data.isnull().sum())  
+data = load_data("Sports_and_Outdoors_5.json") 
 
 cleaned_data = clean_data(data)
 
-print("\nCleaned Dataset Info:")
-print(cleaned_data.info())
+user_item_matrix = create_user_item_matrx(cleaned_data)
+
+print(user_item_matrix.head())
+
